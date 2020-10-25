a2a774ead284c0cd3ee6eda1f0da59418e4ba405
Your Name <1301142860@qq.com>
2020-10-25 19:15:06 +0800

    first commit

---
+++ day1.js
@@ -0,0 +1,48 @@
+console.log('hellow js')
+
+
+let d;
+console.log(typeof d)
+
+
+let a1 ="tian"
+let a2 = "udaskd"
+let a3 = "xsjwi"
+
+const strName = `
+<h1>
+    <p>${a1}</p>
+    <p>${a2}</p>
+    <p>${a3}</p>
+</h1>
+`
+console.log(strName)
+
+const oStudent = {
+    name:'tian',
+    age:18
+    // 创建对象
+}
+console.log('name' in oStudent)
+delete oStudent.name
+console.log('name' in oStudent)
+
+const str1 = [2,3,4,5,6];
+console.log(2 in str1)
+
+const oArray = new Array()
+console.log(oArray instanceof Object)
+// 判断一个对象是不是函数
+
+
+
+
+
+
+
+
+
+
+
+
+
